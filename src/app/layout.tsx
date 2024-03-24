89827124e60635205d83dec28d06269273616eb3
Jagroop Bagri <jagroopbagri5@gmail.com>
2024-03-23 20:27:49 -0700

    incorporate daisyui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css"
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,19 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useState, useRef, useEffect  } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import axios from "axios";
 import { PropagateLoader } from "react-spinners";
-
-
 
 const defaultUserState = { username: "", password: "" };
 
 export default function LoginPage() {
   const router = useRouter();
   const [user, setUser] = useState(defaultUserState);
-  const [buttonDisabled, setButtonDisabled] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
-  const [showForgotPasswordModal, setShowForgotPasswordModal] = useState<boolean>(false);
+  const [forgotEmail, setForgotEmail] = useState<string>("");
+  const [fpModalOpen, setFPModalOpen] = useState<boolean>(false);
+  const fpModalRef = useRef<HTMLDialogElement>(null);
 
   // on login submit
   const onSubmit = async (e: any) => {
@@ -36,65 +35,104 @@ export default function LoginPage() {
   const onForgotPasswordSubmit = async (email: string) => {
     // Implement the API call or logic here to handle the forgot password action
     console.log(email);
-    setShowForgotPasswordModal(false); // Close the modal after submission
+  };
+
+  // This function is used to open the forgot password modal
+  const toggleForgotPasswordModal = () => {
+      setFPModalOpen(!fpModalOpen);
   };
 
   useEffect(() => {
-    if (user.username.length > 0 && user.password.length > 0) {
-      setButtonDisabled(false);
+    if (fpModalOpen) {
+      fpModalRef.current !== null && fpModalRef.current.showModal();
     } else {
-      setButtonDisabled(true);
+      if (fpModalRef.current && typeof fpModalRef.current.close === 'function') {
+        fpModalRef.current.close();
+      }
     }
-  }, [user]);
+  }, [fpModalOpen]);
 
   return (
     <>
-    <div
-      className={
-        "flex flex-col items-center justify-center h-screen p-5 w-full"
-      }
-    >
-      <h1 className={"text-2xl my-16"}>Login</h1>
-      <label htmlFor={"username"}>Username</label>
-      <input
-        className={
-          "text-black my-4 p-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-white"
-        }
-        type={"text"}
-        id={"username"}
-        placeholder={"Username"}
-        value={user.username}
-        onChange={(e) => setUser({ ...user, username: e.target.value })}
-      />
+      <div className={"flex flex-col items-center h-screen p-5 w-full mt-52"}>
+        <h1 className={"text-8xl mb-20"}>Stock Pilot</h1>
+        <input
+          className="input input-bordered w-full max-w-xs mb-6"
+          type={"text"}
+          id={"username"}
+          placeholder={"Username"}
+          value={user.username}
+          onChange={(e) => setUser({ ...user, username: e.target.value })}
+        />
 
-      <label htmlFor={"password"}>Password</label>
-      <input
-        className={
-          "text-black my-4 p-2 rounded border border-gray-300 focus:outline-none focus:ring-2 focus:ring-white"
-        }
-        type={"password"}
-        id={"password"}
-        placeholder={"Password"}
-        value={user.password}
-        onChange={(e) => setUser({ ...user, password: e.target.value })}
-      />
-      <button
-        className={
-          "mt-4 border-2 border-white py-2 rounded hover:bg-white hover:text-black p-2 cursor-pointer"
-        }
-        onClick={onSubmit}
-        disabled={buttonDisabled}
-      >
-        {loading ? <PropagateLoader /> : "Login"}
-      </button>
-      <Link className={"text-sm mt-6"} href={"/sign-up"}>
-        {"Don't have an account? Sign Up"}
-      </Link>
-      <button className="text-sm mt-4" onClick={() => setShowForgotPasswordModal(true)}>
-        Forgot your password?
-      </button>
-    </div>
+        <input
+          className="input input-bordered w-full max-w-xs mb-6"
+          type={"password"}
+          id={"password"}
+          placeholder={"Password"}
+          value={user.password}
+          onChange={(e) => setUser({ ...user, password: e.target.value })}
+        />
+        <button className={"btn bg-success"} onClick={onSubmit}>
+          {loading ? <PropagateLoader /> : "Login"}
+        </button>
+        <Link className={"mb-6 mt-8 link-primary"} href={"/sign-up"}>
+          {"Don't have an account? Sign Up"}
+        </Link>
+
+        <button className="link-primary" onClick={toggleForgotPasswordModal}>
+          Forgot your password?
+        </button>
+        <dialog id="forgot_password_modal" className="modal" ref={fpModalRef}>
+          <div className="modal-box">
+            <form
+              method="dialog"
+              className="w-full flex flex-col items-center"
+              onSubmit={(e) => {
+                e.preventDefault(); // Prevent default form submission
+                onForgotPasswordSubmit(forgotEmail);
+              }}
+            >
+              <button
+                className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+                onClick={(event) => {
+                  event.preventDefault();
+                  toggleForgotPasswordModal();
+                }}
+              >
+                ✕
+              </button>
+              <h2 className="font-bold text-lg text-center mb-6">
+                Recover your account
+              </h2>
+              <input
+                className="input input-bordered w-full max-w-xs mb-6"
+                type="email"
+                id="forgot-email"
+                placeholder="Email"
+                value={forgotEmail}
+                onChange={(e) => setForgotEmail(e.target.value)}
+              />
+              <div className="flex gap-6">
+                <button
+                  type="button"
+                  className="btn bg-error"
+                  onClick={(event) => {
+                    event.preventDefault();
+                    toggleForgotPasswordModal();
+                  }}
+                >
+                  Cancel
+                </button>
+
+                <button type="submit" className="btn bg-success">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </dialog>
+      </div>
     </>
   );
-};
-
+}
